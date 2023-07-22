0df412485a9d67610eebb8f4189c3dac96132120
Alexxanderrx <alexmorales196@gmail.com>
2023-07-22 12:45:52 -0500

    eliminacion de la tabla users, debido a la redundancia inesesaria de datos

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                     <input class="block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="text" name="email" placeholder="Email" />
                 </div>
 
-                <div class="relative block mb-5">
+                <div class="relative block mb-2">
                     <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                         <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
@@ -44,7 +44,7 @@
                 </div>
 
             </div>
-            <p class="text-center  text-red-700">&#160;
+            <p class="text-center mb-1  text-red-700">&#160;
                 <?php
                 session_start();
                 if (isset($_SESSION["error_rg"])) {
